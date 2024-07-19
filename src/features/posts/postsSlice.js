fc99d666c089577d6918726818f757c33848b883
sugama <reiji.sugama@gmail.com>
2024-07-19 17:18:47 +0900

    reducerを追加

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -2,13 +2,19 @@ import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = [
   { id: '1', title: 'First Post!', content: 'Hello!' },
-  { id: '2', title: 'Second Post', content: 'More text' }
+  { id: '2', title: 'Second Post', content: 'More text' },
 ]
 
 const postsSlice = createSlice({
   name: 'posts',
   initialState,
-  reducers: {}
+  reducers: {
+    // 現在の状態とdispatchされたアクションを引数で受け取る
+    // createSliceは自動的に同名のaction creatorを作成する。
+    postAdded(state, action) {
+      state.push(action.payload)
+    },
+  },
 })
 
 export default postsSlice.reducer
